ee893027966d091a2696a8f827f5ef9c603589c1
hyun2 <hks0284@gmail.com>
2021-12-21 18:10:42 +0900

    로그인, 회원가입 UI

--- frontend/src/components/Register/Register.js
+++ frontend/src/components/Register/Register.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import { Button, Input } from "@mantine/core";
+
+export const Wrapper = styled.div`
+  margin: 30px auto;
+  width: 320px;
+  height: 420px;
+`;
+
+export const CInput = styled(Input)`
+  margin-bottom: 15px;
+`;
+
+export const CButton = styled(Button)`
+  width: 100%;
+`;
+
 const Register = () => {
-  const StyledContainer = styled.div`
+  const RegisterContainer = styled.div`
     display: flex;
     justify-content: center;
+    height: calc(100vh - 101px);
   `;
-  return <StyledContainer>회원가입</StyledContainer>;
+
+  return (
+    <RegisterContainer>
+      <Wrapper>
+        <CInput variant="default" placeholder="ID" />
+        <CInput variant="default" placeholder="Password" />
+        <CButton variant="light">회원가입</CButton>
+      </Wrapper>
+    </RegisterContainer>
+  );
 };
 
 export default Register;
