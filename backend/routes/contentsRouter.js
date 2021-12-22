7612844057c0b386869b6d11ecc1a0b60bae6f68
ms3221 <hyeongjun3221@gmail.com>
2021-12-22 23:27:07 +0900

    contents D

---
+++ backend/routes/contentsRouter.js
@@ -0,0 +1,41 @@
+
+
+
+const express = require('express');
+const router  = express.Router();
+const db = require('../models');
+const { Contents } = require("../models");
+
+
+
+
+
+router.post('/',(req,res)=>{
+
+
+    //보내고 받는 형식 한번만 확인해주세요!
+    let data = {
+        userId : req.body.userId,
+        title : req.body.title,
+        content : req.body.content
+    }
+
+
+    
+  Contents.create({
+      userId : data.userId,
+      title : data.title,
+      content : data.content
+  })
+  .then((result)=>{
+      console.log('저장성공!');
+      res.send('저장성공!');
+  })
+    .catch((err)=>{
+        console.log(err);
+        res.send('content 저장 실패! ');
+    })
+    
+})
+
+module.exports = router;
