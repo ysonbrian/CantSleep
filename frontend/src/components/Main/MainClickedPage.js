55a6b171509f393c446b81ed92b1429361019c07
hyun2 <hks0284@gmail.com>
2021-12-28 16:56:37 +0900

    프론트 코드 정리: warning 제거

--- frontend/src/components/Main/MainClickedPage.js
+++ frontend/src/components/Main/MainClickedPage.js
@@ -1,82 +1,83 @@
-import React from 'react';
-import styled from 'styled-components';
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-import { Link } from 'react-router-dom';
-import { BiArrowBack } from 'react-icons/bi';
-const MainClickedPage = ({ clickedItem }) => {
-  const ClickedItemContainer = styled.div`
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    flex-direction: column;
-    font-size: 1rem;
-    line-height: 2.5rem;
-    margin-top: 20px;
-    width: 100%;
-    height: 100vh;
-    padding: 0 10%;
-    a {
-      cursor: pointer;
-      font-size: 40px;
-      color: black;
-      justify-content: ;
-    }
-    background-color: #ffffff;
-  `;
+import React from "react";
+import styled from "styled-components";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { Link } from "react-router-dom";
+import { BiArrowBack } from "react-icons/bi";
 
-  const ClickedItemTitle = styled.p`
+const ClickedItemContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+  font-size: 1rem;
+  line-height: 2.5rem;
+  margin-top: 20px;
+  width: 100%;
+  height: 100vh;
+  padding: 0 10%;
+  a {
+    cursor: pointer;
     font-size: 40px;
-    padding: 30px;
-    width: 100%;
-    text-align: center;
-  `;
+    color: black;
+    justify-content: ;
+  }
+  background-color: #ffffff;
+`;
 
-  const ClickedItemContent = styled.p`
-    text-align: left;
+const ClickedItemTitle = styled.p`
+  font-size: 40px;
+  padding: 30px;
+  width: 100%;
+  text-align: center;
+`;
+
+const ClickedItemContent = styled.p`
+  text-align: left;
+  font-size: 20px;
+  padding: 30px;
+  width: 100%;
+`;
+const ClickedBackButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: white;
+  width: 100%;
+  height: 60px;
+`;
+const ClickedBackButton = styled.button`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  background: white;
+  border: none;
+  border-radius: 4px;
+  font-weight: bold;
+  outline: none;
+  padding: 0px 1.25rem;
+  text-align: center;
+  width: 150px;
+  height: 30px;
+  margin-left: 10px;
+  :hover {
+    background-color: #f4f4f4;
+  }
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+    text-decoration: none;
+    color: black;
+  }
+
+  p {
     font-size: 20px;
-    padding: 30px;
-    width: 100%;
-  `;
-  const ClickedBackButtonContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color: white;
-    width: 100%;
-    height: 60px;
-  `;
-  const ClickedBackButton = styled.button`
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    background: white;
-    border: none;
-    border-radius: 4px;
-    font-weight: bold;
-    outline: none;
-    padding: 0px 1.25rem;
-    text-align: center;
-    width: 150px;
-    height: 30px;
-    margin-left: 10px;
-    :hover {
-      background-color: #f4f4f4;
-    }
-    a {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      gap: 8px;
-      text-decoration: none;
-      color: black;
-    }
+  }
+`;
 
-    p {
-      font-size: 20px;
-    }
-  `;
-
+const MainClickedPage = ({ clickedItem }) => {
   const info = `# ${clickedItem.title}
   \
   \
